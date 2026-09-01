809dbbb1445354f68f2a14103dc9f0df7fca8ab5
Lucas Cordeiro <105241465+IoStracciatella@users.noreply.github.com>
2026-09-01 10:47:09 -0300

    começando a implementar as funções

---
+++ polinomios.h
@@ -0,0 +1,25 @@
+void NOME ();
+
+void SOMA ();
+
+void PRODUTO ();
+
+void ADD ();
+
+void ESCALA ();
+
+int COEF ();
+
+void REMOVE ();
+
+void REMOVEMENOR ();
+
+int GRAU ();
+
+float IMPRIME[];
+
+float IMPRIMEINV[];
+
+void LIBERA ();
+
+void FIM ();
